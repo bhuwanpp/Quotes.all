db7d90d6d8a83142e8af990977ec8d99f8ab6533
Bhuwan <bhubanpaudel79@gmail.com>
2024-02-15 13:47:30 +0545

    add use client

--- app/create/page.tsx
+++ app/create/page.tsx
@@ -1,3 +1,4 @@
+'use client'
 import { useState } from "react"
 import supabase from "../config/supabaseClient"
 import { useRouter } from "next/navigation"
--- app/yourQuotes/[update]/page.tsx
+++ app/yourQuotes/[update]/page.tsx
@@ -1,3 +1,4 @@
+'use client'
 import {  useParams, useRouter } from "next/navigation"
 import { useEffect, useState } from "react"
 import supabase from "@/app/config/supabaseClient"
